7edc2de92523007ca351f0283f7a7fa49caacf2c
Nicolas Claverie <info@artscore-studio.fr>
2016-03-11 10:31:25 +0100

    Bug fix typo in namespace

--- Form/Factory/FactoryInterface.php
+++ Form/Factory/FactoryInterface.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace ASW\ContactBundle\Form\Factory;
+namespace ASF\ContactBundle\Form\Factory;
 
 /**
  * Contact Bundle form factory interface
