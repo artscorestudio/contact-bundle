39bf09b429eddd83469da0d24fdfaece65ab5508
Nicolas Claverie <info@artscore-studio.fr>
2016-04-11 16:34:54 +0200

    Change use statement to ASFEntityManager

--- Utils/Manager/DefaultEntityManager.php
+++ Utils/Manager/DefaultEntityManager.php
@@ -9,7 +9,7 @@
  */
 namespace ASF\ContactBundle\Utils\Manager;
 
-use ASF\CoreBundle\Entity\Manager\ASFEntityManager;
+use ASF\CoreBundle\Utils\Manager\ASFEntityManager;
 
 /**
  * Generic Entity Manager for this bundle
